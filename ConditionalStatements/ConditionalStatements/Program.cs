d92b994d9ac7ae3d599b86c6ae58c22b6affeb38
Lowry West <4lowrywest@gmail.com>
2021-02-08 13:12:49 -0600

    Conditional Statements

---
+++ ConditionalStatements/ConditionalStatements/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConditionalStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int luckyNumber;
+            Console.WriteLine("What is your favorite number? (Whole numbers only!)");
+            luckyNumber = Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
