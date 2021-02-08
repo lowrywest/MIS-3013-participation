0dc8a56a39fcc1e658e1f356b58f3281da7d23be
Lowry West <4lowrywest@gmail.com>
2021-02-08 13:35:50 -0600

    Switch statment added

--- ConditionalStatements/ConditionalStatements/Program.cs
+++ ConditionalStatements/ConditionalStatements/Program.cs
@@ -7,9 +7,19 @@ namespace ConditionalStatements
         static void Main(string[] args)
         {
             int luckyNumber;
+            string answer;
             Console.WriteLine("What is your favorite number? (Whole numbers only!)");
-            luckyNumber = Convert.ToInt32(Console.ReadLine());
+            answer = Console.ReadLine();
+            bool isInteger = int.TryParse(answer, out luckyNumber);
+            if (isInteger == false)
+            {
+                Console.WriteLine(luckyNumber+" is not a valid integer. Goodbye!");
+                Environment.Exit(0);
+                //can also use the return; to exit the application
+            }
+           
             int remainder = luckyNumber % 7;
+
             if (remainder == 0)
             {
                 Console.WriteLine(luckyNumber + " is truly a lucky number!");
@@ -22,7 +32,23 @@ namespace ConditionalStatements
             {
                 Console.WriteLine(luckyNumber);
             }
-            
+
+            Console.WriteLine("What is your favorite type of pet?");
+            answer = Console.ReadLine();
+
+            switch (answer.ToLower())
+            {
+                case "dog":
+                    Console.WriteLine("Cool me too!");
+                    break;
+                case "cat":
+                    Console.WriteLine("If you say so");
+                    break;
+                default:
+                    Console.WriteLine("Who would ever have " + answer + " as a pet?");
+                    break;
+            }
+
         }
     }
 }
