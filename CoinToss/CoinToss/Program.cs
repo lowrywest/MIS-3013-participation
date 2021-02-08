a6cf75064b8fdcfec7d36439734752dc7eb49889
Lowry West <4lowrywest@gmail.com>
2021-02-08 13:48:46 -0600

    Coin flip done

--- CoinToss/CoinToss/Program.cs
+++ CoinToss/CoinToss/Program.cs
@@ -6,7 +6,27 @@ namespace CoinToss
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random rand = new Random();
+            int randomNbr = rand.Next(1,3);
+            //Console.WriteLine(randomNbr);
+            Console.WriteLine("Guess heads or tails!");
+            String ans = Console.ReadLine().ToLower();
+            if (ans.Equals("heads") && randomNbr == 1)
+            {
+                Console.WriteLine("The coin landed on heads and you guessed correct!");
+            }
+            else if (ans.Equals("heads") && randomNbr == 2)
+            {
+                Console.WriteLine("The coin landed on heads but you guessed wrong.");
+            }
+            else if (ans.Equals("tails") && randomNbr == 1)
+            {
+                Console.WriteLine("The coin landed on tails but you guessed wrong.");
+            }
+            else
+            {
+                Console.WriteLine("The coin landed on tails and you guessed correct!");
+            }
         }
     }
 }
