2c8c6924a058d503a50cd1581fb0ec88af10ecef
Lowry West <4lowrywest@gmail.com>
2021-02-08 13:19:55 -0600

    Conditonal statements completed

--- ConditionalStatements/ConditionalStatements/Program.cs
+++ ConditionalStatements/ConditionalStatements/Program.cs
@@ -9,7 +9,20 @@ namespace ConditionalStatements
             int luckyNumber;
             Console.WriteLine("What is your favorite number? (Whole numbers only!)");
             luckyNumber = Convert.ToInt32(Console.ReadLine());
-
+            int remainder = luckyNumber % 7;
+            if (remainder == 0)
+            {
+                Console.WriteLine(luckyNumber + " is truly a lucky number!");
+            }
+            else if (luckyNumber % 13 == 0)
+            {
+                Console.WriteLine(luckyNumber + " is a really unlucky number!");
+            }
+            else
+            {
+                Console.WriteLine(luckyNumber);
+            }
+            
         }
     }
 }
