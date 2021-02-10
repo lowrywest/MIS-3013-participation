58e0725595468a9052f0083477a88714ed7a6957
Lowry West <4lowrywest@gmail.com>
2021-02-10 12:55:01 -0600

    Fizz Buzz

--- CoinToss/CoinToss/Program.cs
+++ CoinToss/CoinToss/Program.cs
@@ -6,6 +6,7 @@ namespace CoinToss
     {
         static void Main(string[] args)
         {
+            const String devName = "Lowry West";
             Random rand = new Random();
             int randomNbr = rand.Next(1,3);
             //Console.WriteLine(randomNbr);
@@ -13,20 +14,22 @@ namespace CoinToss
             String ans = Console.ReadLine().ToLower();
             if (ans.Equals("heads") && randomNbr == 1)
             {
-                Console.WriteLine("The coin landed on heads and you guessed correct!");
+                Console.WriteLine("The coin landed on heads so you guessed correct!");
             }
             else if (ans.Equals("heads") && randomNbr == 2)
             {
-                Console.WriteLine("The coin landed on heads but you guessed wrong.");
+                Console.WriteLine("The coin landed on tails so you guessed wrong.");
             }
             else if (ans.Equals("tails") && randomNbr == 1)
             {
-                Console.WriteLine("The coin landed on tails but you guessed wrong.");
+                Console.WriteLine("The coin landed on heads so you guessed wrong.");
             }
             else
             {
-                Console.WriteLine("The coin landed on tails and you guessed correct!");
+                Console.WriteLine("The coin landed on tails so you guessed correct!");
             }
+
+            Console.WriteLine("The developer's name is "+devName);
         }
     }
 }
---
+++ FizzBuzz_ConditionalStatements/FizzBuzz_ConditionalStatements/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FizzBuzz_ConditionalStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int randomNumberBetween1and100 = rand.Next(1, 101);
+
+            if (randomNumberBetween1and100 % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (randomNumberBetween1and100 % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else if (randomNumberBetween1and100 % 3 == 0 && randomNumberBetween1and100 % 5 == 0)
+            {
+                Console.WriteLine("Fizz Buzz");
+            }
+        }
+    }
+}
