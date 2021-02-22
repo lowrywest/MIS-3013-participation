c557f81018eb149bf2dc09440431a6f4f8366fe5
Lowry West <4lowrywest@gmail.com>
2021-02-22 13:37:34 -0600

    Loops practice

--- Loops/Loops/Program.cs
+++ Loops/Loops/Program.cs
@@ -6,7 +6,44 @@ namespace Loops
     {
         static void Main(string[] args)
         {
+
+            WhileExample();
+           // DoWhileExample();
+           // ForLoopExample();
+        
+
             
+        }
+        private static void WhileExample()
+        {
+            int favNum;
+            Console.WriteLine("What is your favorite number?");
+            string ans = Console.ReadLine();
+            while (int.TryParse(ans, out favNum) == false)
+            {
+                Console.WriteLine("Incorrect Value. you murst enter an Integer.");
+                ans = Console.ReadLine();
+            }
+        }
+        private static void DoWhileExample()
+        {
+            string usersAnswer = "";
+            do
+            {
+                Console.WriteLine("Knock Knock. Who's there?");
+                usersAnswer = Console.ReadLine();
+
+            } while (usersAnswer.ToLower() != "banana");
+
+
+        }
+        private static void ForLoopExample()
+        {
+            for (int i = 3; i <= 1000; i = i + 3)
+            {
+                Console.WriteLine(i);
+            }
+
         }
     }
 }
