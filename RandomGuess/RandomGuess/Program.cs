447ba3632199ed75c4615b595aad812aa436db9a
Lowry West <4lowrywest@gmail.com>
2021-03-01 12:58:24 -0600

    String manipulation

---
+++ RandomGuess/RandomGuess/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace RandomGuess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randNum = new Random();
+            int lowNum, highNum, guess, rand;
+            Console.WriteLine("This is a random number guessing game. Good Luck");
+            Console.WriteLine("Please enter the low bound in the range ");
+            lowNum=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the high bound in the range ");
+            highNum = Convert.ToInt32(Console.ReadLine());
+            rand = (int)randNum.Next(lowNum, highNum);
+            do
+            {
+                Console.WriteLine("Enter your guess");
+                guess = Convert.ToInt32(Console.ReadLine());
+                if (guess == rand)
+                {
+                    Console.WriteLine("You guessed correctly!");
+                }
+                else
+                {
+                    Console.WriteLine("Try again.");
+                }
+
+            } while (guess != rand);
+
+        }
+    }
+}
---
+++ StringManipulation/StringManipulation/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StringManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Sent = "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, " +
+                "and the universe trying to build bigger and better idiots. So far, the universe is winning.";
+        }
+    }
+}
