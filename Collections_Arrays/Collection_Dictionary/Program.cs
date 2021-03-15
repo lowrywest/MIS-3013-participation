f72c5d36658776d910343f168e4513aa7de8b051
Lowry West <4lowrywest@gmail.com>
2021-03-15 13:55:38 -0500

    in class

--- Collections_Arrays/Collection_Dictionary/Program.cs
+++ Collections_Arrays/Collection_Dictionary/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace Collection_Dictionary
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, double> students = new Dictionary<string, double>();
+            students.Add("1", 3.0);
+            students.Add("2", 4.0);
+            students.Add("3", 2.5);
+            Console.WriteLine("Do you want to add another student?");
+            string ans = Console.ReadLine();
+            if (ans.ToLower()[0] == 'y')
+            {
+                Console.WriteLine("What is the students ID?");
+                string id = Console.ReadLine();
+                Console.WriteLine("What is the students GPA?");
+                double gpa = Convert.ToDouble(Console.ReadLine());
+                if (students.ContainsKey(id) == true)
+                {
+                    Console.WriteLine("That student is already in the system. We will update the GPA");
+                    students[id] = gpa;
+                }
+                else
+                {
+                    students.Add(id, gpa);
+                }
+            }
+
+            foreach (string id in students.Keys)
+            {
+                Console.WriteLine(id);
+            }
+            Console.WriteLine("Whose GPA do you want to see?");
+            string searchID = Console.ReadLine();
+            if (students.ContainsKey(searchID) == true)
+            {
+                Console.WriteLine(searchID + " " + students[searchID].ToString("N3"));
+            }
+           
+            
         }
     }
 }
---
+++ Collections_Arrays/Collections_Lists/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> favoriteThings = new List<string>();
+            string ans;
+            do
+            {
+                Console.WriteLine("Name one of your favorite things");
+                ans = Console.ReadLine();
+                favoriteThings.Add(ans);
+                Console.WriteLine("do you have another favorite thing?");
+                ans = Console.ReadLine();
+            }while (ans.ToLower()[0] == 'y');
+
+            Random rand = new Random();
+            int index = rand.Next(0,favoriteThings.Count);
+
+            Console.WriteLine(favoriteThings[index]);
+        }  
+    }
+}
