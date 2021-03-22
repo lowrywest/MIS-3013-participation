792a6cd9e8df06e5b6bbefe199f6cec14498b9d2
Lowry West <4lowrywest@gmail.com>
2021-03-22 13:28:38 -0500

    complete

--- Collections_Min-Max-AVG/Collections_Min-Max-AVG/Program.cs
+++ Collections_Min-Max-AVG/Collections_Min-Max-AVG/Program.cs
@@ -7,8 +7,77 @@ namespace Collections_Min_Max_AVG
     {
         static void Main(string[] args)
         {
+            List<double> testScore = new List<double>();
+            Dictionary <double, int> modeCalc = new Dictionary<double, int>();
+            double max, min, avg;
+            double mode=0;
+            double sum =0;
+            double maxTemp, minTemp;
+            bool fin = true;
+            do
+            {
+                Console.WriteLine("Please enter your exam scores. Enter the word done when complete.");
+                string input = Console.ReadLine();
+                if (input[0] == 'd')
+                {
+                    fin = false;
+                }
+                else 
+                {
+                    double score = Convert.ToDouble(input);
+                    testScore.Add(score);
+                }
+
+            } while (fin==true);
 
 
+            minTemp = testScore[0];
+            maxTemp = testScore[0];
+            for (int i = 0; i < testScore.Count; i++)
+            {
+
+                sum = testScore[i] + sum;
+                if (maxTemp < testScore[i])
+                {
+                    maxTemp = testScore[i];
+                }
+                if (minTemp > testScore[i])
+                {
+                    minTemp = testScore[i];
+                }
+                if (modeCalc.ContainsKey(testScore[i]) == true)
+                {
+                    modeCalc[testScore[i]] = modeCalc[testScore[i]] + 1;
+                }
+                else 
+                {
+                    modeCalc.Add(testScore[i], 1);
+                }
+                
+            }
+            foreach (double scoreKey in modeCalc.Keys)
+            {
+                if (modeCalc[scoreKey] > mode)
+                {
+                    mode = modeCalc[scoreKey];
+                }
+            }
+            
+            
+            avg = sum / testScore.Count;
+            max = maxTemp;
+            min = minTemp;
+            Console.WriteLine("The highest exam score is "+max+".");
+            Console.WriteLine("The lowest exam score is " + min+".");
+            Console.WriteLine("The average exam score is " + avg+".");
+
+            foreach (double scoreKey in modeCalc.Keys)
+            {
+                if (modeCalc[scoreKey] == mode)
+                {
+                    Console.WriteLine("The grade " + scoreKey +" occurs the most often. ");
+                }
+            }
         }
     }
 }
---
+++ File_extracting_Data/File_extracting_Data/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace File_extracting_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] lines = File.ReadAllLines("geoMap.csv");
+            List<string> states = new List<string>();
+            List<double> soccer = new List<double>();
+            List<double> football = new List<double>();
+
+            for (int i =0; i <lines.Length; i++)
+            {
+                string line = lines[i];
+                Console.WriteLine(line);
+                string[] pieces = line.Split(',');
+                string state = pieces[0];
+                states.Add(state);
+
+                string soccerScoreAsString = pieces[1];
+                double soccerScore = Convert.ToDouble(soccerScoreAsString.Trim('%'))/100;
+                soccer.Add(soccerScore);
+                football.Add(Convert.ToDouble(pieces[2].Trim('%' )) / 100);
+            }
+
+            for (int i = 0; i <football.Count;i++)
+            { 
+                if(soccer[])
+            }
+        }
+    }
+}
