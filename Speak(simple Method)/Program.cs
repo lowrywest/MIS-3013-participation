509c861105f41f4949f9be8364316c9ba40da73d
Lowry West <4lowrywest@gmail.com>
2021-04-08 16:35:58 -0500

    speak

---
+++ Speak(simple Method)/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Speak_simple_Method_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the animal you want to see the sound of.");
+            string input = Console.ReadLine().Trim();
+            string sound = Speak(input);
+            Console.WriteLine("A "+input+" makes the sound of "+sound);
+        }
+        static string Speak(string animal)
+        {
+            string output = "booooo";
+            string[] animals = { "dog", "cat", "horse", "pig","lion","cow" };
+            string[] sounds = { "ruff", "meow", "neigh", "oink", "roar", "mooo" };
+            for (int i = 0; i < animals.Length; i++)
+            {
+                if (animals[i] == animal.ToLower())
+                {
+                    output = sounds[i];
+                }
+            }
+            return output;
+        }
+    }
+}
