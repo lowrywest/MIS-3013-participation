dad36ca522c414535f3c5a3c212e9657b90df114
Lowry West <4lowrywest@gmail.com>
2021-04-12 13:58:03 -0500

    updates

--- InClass_ClassExample/Program.cs
+++ InClass_ClassExample/Program.cs
@@ -8,15 +8,21 @@ namespace InClass_ClassExample
         {
 
             Rectangle rect = new Rectangle();
+            Rectangle rect2 = new Rectangle(3, 4);
             rect.Length = 10;
             rect.Width = 15;
             Circle circ = new Circle();
+            Circle circ2 = new Circle(3.32);
             circ.Radius = 5.0;
 
             Console.WriteLine("The area of your rectangle is "+rect.CalculateArea().ToString("N2"));
             Console.WriteLine("The perimeter of your rectangle is " + rect.CalculatePerimeter().ToString("N2"));
+            Console.WriteLine("The area of your rectangle is " + rect2.CalculateArea().ToString("N2"));
+            Console.WriteLine("The perimeter of your rectangle is " + rect2.CalculatePerimeter().ToString("N2"));
             Console.WriteLine("The area of your circle is " + circ.CalculateArea().ToString("N2"));
-            Console.WriteLine("The perimeter of your circle is " +circ.CalculatePerimeter().ToString("N2"));
+            Console.WriteLine("The perimeter of your circle is " + circ.CalculatePerimeter().ToString("N2"));
+            Console.WriteLine("The area of your circle is " + circ2.CalculateArea().ToString("N2"));
+            Console.WriteLine("The perimeter of your circle is " +circ2.CalculatePerimeter().ToString("N2"));
 
 
         }
