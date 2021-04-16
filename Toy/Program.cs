5e89f29783df8f7aecc7e71332f47b61a3cd9b7f
Lowry West <4lowrywest@gmail.com>
2021-04-16 12:00:37 -0500

    toy

---
+++ Toy/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Toy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ToyClass one = new ToyClass("Sony","PlayStation",450,"Less shipping problems ");
+            ToyClass two = new ToyClass("Microsoft", "Xbox", 500, "More shipping problems ");
+            Console.WriteLine(one.ToString());
+            Console.WriteLine(one.GetAisle());
+            Console.WriteLine(two.ToString());
+            Console.WriteLine(two.GetAisle());
+
+            
+        }
+    }
+}
---
+++ Toy/ToyClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Toy
+{
+    class ToyClass
+    {
+        public string Manufactuer { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        private string Notes { get; set; }
+        public ToyClass()
+        {
+            Manufactuer = string.Empty;
+            Name = string.Empty;
+            Price = 0;
+            Notes = string.Empty;
+        
+        }
+        public ToyClass(string man, string name, double price, string notes)
+        {
+            Manufactuer = man;
+            Name = name;
+            Price = price;
+            Notes = notes;
+        }
+
+        public string GetAisle()
+        {
+            Random rand = new Random();
+            int randNum = rand.Next(1, 25);
+            string temp = Manufactuer.ToUpper();
+            temp = temp[0] + randNum.ToString();
+            return temp;
+        
+        }
+        public override string ToString()
+        {
+            return "Manufacurer: "+ Manufactuer+" Name: "+Name+" Price: "+Price+" Notes: "+Notes;
+        }
+    }
+}
